c667d140b92416fd67b59809c0e1147be0b0b97c
Roberto Oliveira <robertooliveiragp@gmail.com>
2022-05-11 10:15:08 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { GlobalStyle } from './styles/global';
+
+import { Main } from './pages/Main';
+
+function App() {
+  return (
+    <>
+      <Main />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
