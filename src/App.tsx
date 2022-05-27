266c3f7a2c6b8167ef1ae2007a2c53536768a63c
LUIS ROBERTO OLIVEIRA SANTOS <luis.oliveira@equatorialenergia.com.br>
2022-05-27 10:31:56 -0300

    fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import { AppProvider } from './hooks';
-import { ImageCapture } from './pages/ImageCapture';
 import { GlobalStyle } from './styles/global';
 
 import Routes from './routes';
